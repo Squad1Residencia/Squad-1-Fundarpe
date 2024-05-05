86c2afbfa548a29be153f8806ab9f0706d13f9b3
HugoORamos <HugoOli.Ramos@gmail.com>
2024-05-04 22:41:15 -0300

    Ajustei a estrutura do projeto
    Criei os arquivos, html, views e urls para cada setor  e ajustei o root para que o projeto continue rodando.

--- sg_fundarpe/home/urls/views.py
+++ sg_fundarpe/home/urls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Projeto
+from ..models import Projeto
 #from .forms import ProjetoForm
 
 
--- sg_fundarpe/sg_fundarpe/settings.py
+++ sg_fundarpe/sg_fundarpe/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'sg_fundarpe.urls'
+ROOT_URLCONF = 'home.urls.urls'
 
 TEMPLATES = [
     {
