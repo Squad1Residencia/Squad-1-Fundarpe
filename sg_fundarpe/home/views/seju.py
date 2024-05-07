095e8143594f1c8525fc8636c4ae4af79f06cdbd
HugoORamos <HugoOli.Ramos@gmail.com>
2024-05-06 23:47:20 -0300

    Criei a estrutura de todos os setores

---
+++ sg_fundarpe/home/views/seju.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+from home.models import Projeto #chamar tabelas que serão utilizadas
+from django.http import HttpResponse
+
+def homepage(request):
+    projetos = Projeto.objects.all() 
+    return render(request, 'home/seju.html', {'projetos': projetos})
