96739f21a521f017cbe8cb48ae1e271330ec82a2
Hugo Oliveira Ramos <HugoOli.Ramos@gmail.com>
2024-05-19 16:53:29 -0300

    unat quase finalizado
    falta ajeitar as operacoes

--- sg_fundarpe/home/views/unat.py
+++ sg_fundarpe/home/views/unat.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseForbidden, HttpResponse
+from django.http import HttpResponseForbidden, HttpResponse, JsonResponse 
 from home.models import Projeto, Pagamento
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 @login_required
 def homepage(request):
@@ -25,31 +25,47 @@ def cadastrar_projeto(request):
         valor_solicitado = request.POST.get('valor_solicitado')
         n_parcelas = request.POST.get('n_parcelas')
 
-        novo_projeto = Projeto.objects.create(n_projeto=n_projeto, titulo_projeto=titulo_projeto, n_sei=n_sei)
-        novo_pagamento = Pagamento.objects.create(n_projeto=novo_projeto, valor_solicitado=valor_solicitado, n_parcelas=n_parcelas)
-        
+        novo_projeto, created = Projeto.objects.get_or_create(
+            n_projeto=n_projeto,
+            defaults={'titulo_projeto': titulo_projeto, 'n_sei': n_sei},
+        )
+
+        if not created:
+            novo_projeto.titulo_projeto = titulo_projeto
+            novo_projeto.n_sei = n_sei
+            novo_projeto.save()
+
+        novo_pagamento, created = Pagamento.objects.get_or_create(
+            n_projeto=novo_projeto,
+            defaults={'valor_solicitado': valor_solicitado, 'n_parcelas': n_parcelas},
+        )
+
+        if not created:
+            novo_pagamento.valor_solicitado = valor_solicitado
+            novo_pagamento.n_parcelas = n_parcelas
+            novo_pagamento.save()
+
         return redirect('unat')
     else:
         return HttpResponse("Erro ao cadastrar o projeto", status=400)
 
 @login_required
-def atualizar_projeto(request, n_projeto):
-    # Verificar se o usuário pertence ao departamento 'unat'
-    if request.user.departamento != 'unat':
-        return HttpResponseForbidden()
+def editar_projeto(request, n_projeto):
+    # Busca o projeto com base no n_projeto
+    projeto = get_object_or_404(Projeto, n_projeto=n_projeto)
 
-    if request.method == 'POST':
-        n_projeto = request.POST.get('n_projeto')
-        titulo_projeto = request.POST.get('titulo_projeto')
-        n_sei = request.POST.get('n_sei')
-        valor_solicitado = request.POST.get('valor_solicitado')
-        n_parcelas = request.POST.get('n_parcelas')
+    # Busca o pagamento associado ao projeto
+    pagamento = get_object_or_404(Pagamento, n_projeto=projeto)
 
-        projeto = Projeto.objects.get(id=n_projeto)
-        projeto.n_projeto = n_projeto
-        projeto.titulo_projeto = titulo_projeto
-        projeto.n_sei = n_sei
-        projeto.save()
+    # Cria um dicionário com os dados do projeto e do pagamento
+    data = {
+        'n_projeto': projeto.n_projeto,
+        'titulo_projeto': projeto.titulo_projeto,
+        'n_sei': projeto.n_sei,
+        'valor_solicitado': pagamento.valor_solicitado,
+        'n_parcelas': pagamento.n_parcelas,
+    }
 
-        pagamento = Pagamento.objects.get(n_projeto_id=n_projeto)
-        pagamento.valor_solicitado = valor_solicitado
+    # Retorna os dados como JSON
+    return JsonResponse(data)
+    
