6af4ab592dc772e0ba17f407366c3864730d2c50
gabrielmaia220 <joao.gmatos@souunit.com.br>
2024-05-19 17:32:07 -0300

    Forma uaff

--- sg_fundarpe/home/urls/uaff.py
+++ sg_fundarpe/home/urls/uaff.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from home.views.presi import *
+from home.views.uaff import *
 
 urlpatterns = [
        path('', homepage, name = 'uaff'),
+       path('cadastro_uaff/', cadastro_uaff , name='cadastro_uaff'),
+       path('cadastro_uaff1/', cadastro_uaff1 , name='cadastro_uaff1'),
 
 ]       
--- sg_fundarpe/home/views/uaff.py
+++ sg_fundarpe/home/views/uaff.py
@@ -1,7 +1,46 @@
 from django.shortcuts import render, redirect
-from home.models import Projeto #chamar tabelas que serão utilizadas
+from home.models import Projeto, Operacao #chamar tabelas que serão utilizadas
 from django.http import HttpResponse
 
 def homepage(request):
     projetos = Projeto.objects.all() 
     return render(request, 'home/uaff.html', {'projetos': projetos})
+
+def cadastro_uaff(request):
+    if request.method == 'POST':
+        # Obtém os dados do formulário
+        n_projeto = request.POST.get('n_projeto')
+        nome_operacao = request.POST.get('nome_operacao')
+        data_operacao = request.POST.get('data_operacao')
+        
+        projeto = Projeto.objects.filter(n_projeto=n_projeto).first()
+        if not projeto:
+            return HttpResponse("Projeto não encontrado", status=400)
+
+
+        projeto.save()
+        Operacao.objects.create(n_projeto=projeto, id_usuario=request.user, data_operacao=data_operacao, status_operacao='Concluida', nome_operacao=nome_operacao)
+        return redirect('uaff')
+    else:
+        return HttpResponse("Erro ao cadastrar a operação", status=400)
+    
+def cadastro_uaff1(request):
+    if request.method == 'POST':
+        # Obtém os dados do formulário
+        n_projeto = request.POST.get('n_projeto')
+        nome_operacao = request.POST.get('nome_operacao')
+        data_envio = request.POST.get('data_envio')
+        data_recebimento = request.POST.get('data_recebimento')
+        nome_envio = request.POST.get('nome_envio')
+        
+        projeto = Projeto.objects.filter(n_projeto=n_projeto).first()
+        if not projeto:
+            return HttpResponse("Projeto não encontrado", status=400)
+
+
+        projeto.save()
+        Operacao.objects.create(n_projeto=projeto, id_usuario=request.user, status_operacao='Concluida', nome_operacao=nome_operacao,data_operacao=data_recebimento)
+        Operacao.objects.create(n_projeto=projeto, id_usuario=request.user, status_operacao='Concluida',data_operacao=data_envio,nome_operacao=nome_envio )
+        return redirect('uaff')
+    else:
+        return HttpResponse("Erro ao cadastrar a operação", status=400)
