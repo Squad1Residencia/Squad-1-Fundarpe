3fa814bfb6aee8d4ab64cd7a088c6e03f5c45bc0
Hugo Oliveira Ramos <HugoOli.Ramos@gmail.com>
2024-05-19 23:19:14 -0300

    form uaff
    alterei para salvar o status do projeto

--- sg_fundarpe/home/views/uaff.py
+++ sg_fundarpe/home/views/uaff.py
@@ -17,6 +17,11 @@ def cadastro_uaff(request):
         if not projeto:
             return HttpResponse("Projeto não encontrado", status=400)
 
+        # Define o status do projeto com base na operação
+        if nome_operacao == 'Conferência da Documentação':
+            projeto.status_projeto = '2'
+        elif nome_operacao == 'Envio para Confecção do Termo':
+            projeto.status_projeto = '5'
 
         projeto.save()
         Operacao.objects.create(n_projeto=projeto, id_usuario=request.user, data_operacao=data_operacao, status_operacao='Concluida', nome_operacao=nome_operacao)
@@ -37,8 +42,12 @@ def cadastro_uaff1(request):
         if not projeto:
             return HttpResponse("Projeto não encontrado", status=400)
 
+        # Define o status do projeto para a terceira operação
+        if nome_operacao == 'Recebimento doc assinado':
+            projeto.status_projeto = '8'
 
         projeto.save()
+        # Resto do código...
         Operacao.objects.create(n_projeto=projeto, id_usuario=request.user, status_operacao='Concluida', nome_operacao=nome_operacao,data_operacao=data_recebimento)
         Operacao.objects.create(n_projeto=projeto, id_usuario=request.user, status_operacao='Concluida',data_operacao=data_envio,nome_operacao=nome_envio )
         return redirect('uaff')
