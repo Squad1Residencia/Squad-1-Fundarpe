9238467efd7dad485888046b113b25b887e45e22
Hugo Oliveira Ramos <HugoOli.Ramos@gmail.com>
2024-05-23 23:23:37 -0300

    Update settings.py
    tentando dnv

--- sg_fundarpe/sg_fundarpe/settings.py
+++ sg_fundarpe/sg_fundarpe/settings.py
@@ -11,17 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
 STATICFILES_DIRS = [
     BASE_DIR / 'login/static',
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
